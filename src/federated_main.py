03713e5a5164fec4c0632901cb7cec83232842d1
hoangdzung <hoangdzung@github.com>
2022-05-06 16:42:41 +0200

    minor typo

--- src/federated_main.py
+++ src/federated_main.py
@@ -15,7 +15,7 @@ from tensorboardX import SummaryWriter
 
 from options import args_parser
 from update import LocalUpdate, test_inference
-from models import MLP, CNNMnist, CNNFashion_Mnist, CNNCifar
+from models import MLP, CNNMnist, CNNFashion_Mnist, CNNCifar, BCNNMnist
 from utils import get_dataset, average_weights, exp_details
 
 
