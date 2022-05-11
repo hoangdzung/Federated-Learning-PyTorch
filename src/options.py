437627670b402e172843d5e4a6465fd92f966f29
hoangdzung <hoangdzung@github.com>
2022-05-11 08:38:17 +0200

    add profiler option

--- src/options.py
+++ src/options.py
@@ -70,5 +70,6 @@ def args_parser():
                         help='rounds of early stopping')
     parser.add_argument('--verbose', type=int, default=1, help='verbose')
     parser.add_argument('--seed', type=int, default=1, help='random seed')
+    parser.add_argument('--profiler', action='store_true')
     args = parser.parse_args()
     return args
